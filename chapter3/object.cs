ded7e1386f752aa4845c0a95e9e34369155e32ff
yyujjinn <yujin@ati2000.co.kr>
2023-08-07 14:29:05 +0900

    stack

---
+++ chapter3/object.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class Stack
+{
+    int position;
+    object[] data = new object[10];
+    public void Push(object obj) { data[position++] = obj; }
+    public object Pop() { return data[--position]; }
+
+    static void Main()
+    {
+        Stack stack = new Stack();
+        stack.Push("sausage");
+        string s = (string)stack.Pop();                         // 하향 캐스팅으로 명시적 캐스팅 필요
+
+        Console.WriteLine(s);
+    }
+}
