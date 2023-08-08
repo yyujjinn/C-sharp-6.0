1605a9fa6d097a4278298cf53e81c61b9addb189
yyujjinn <yujin@ati2000.co.kr>
2023-08-08 10:01:20 +0900

    Interface Delegate

---
+++ chapter4/interface_delegate.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+public interface ITransformer
+{
+    int Transform(int x);
+}
+
+public class Util
+{
+    public static void TransformAll(int[] values, ITransformer t)
+    {
+        for (int i = 0; i < values.Length; i++)
+            values[i] = t.Transform(values[i]);
+    }
+}
+
+class Squarer : ITransformer
+{
+    public int Transform(int x) => x * x;
+}
+
+class Test
+{
+    static void Main()
+    {
+    int[] values = { 1, 2, 3 };
+    Util.TransformAll(values, new Squarer());
+    foreach (int i in values)
+        Console.WriteLine(i);
+    }
+}
