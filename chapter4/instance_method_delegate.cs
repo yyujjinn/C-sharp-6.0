f2c699db435496699ac7a433335b94e2e08a03bb
yyujjinn <yujin@ati2000.co.kr>
2023-08-08 09:51:29 +0900

    Instance Method Delegate

---
+++ chapter4/instance_method_delegate.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public delegate void ProgressReporter(int percentComplete);
+
+class Test
+{
+    static void Main()
+    {
+        X x = new X();
+        ProgressReporter p = x.InstanceProgress;
+        p(99);                                          // 99
+        Console.WriteLine(p.Target == x);               // True
+        Console.WriteLine(p.Method);                    // Void InstanceProgress(Int32)
+    }
+}
+
+class X
+{
+    public void InstanceProgress(int percentComplete)
+        => Console.WriteLine(percentComplete);
+}
