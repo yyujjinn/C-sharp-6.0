5e12152d9711f650eb476a85c5b52023f2c73be0
yyujjinn <yujin@ati2000.co.kr>
2023-08-08 11:40:29 +0900

    Try ~ Catch

---
+++ chapter4/try_catch.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Test
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            byte b = byte.Parse(args[0]);
+            Console.WriteLine(b);
+        }
+        catch(IndexOutOfRangeException ex)
+        {
+            Console.WriteLine("인수를 적어도 하나는 지정하세요.");
+        }
+        catch(FormatException ex)
+        {
+            Console.WriteLine("수가 아닙니다!");
+        }
+        catch(OverflowException ex)
+        {
+            Console.WriteLine("byte 하나에 담지 못할 정도로 큰 값!");
+        }
+    }
+}
