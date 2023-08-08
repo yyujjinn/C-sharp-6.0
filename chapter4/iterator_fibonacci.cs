c334a4bc21039229939b0a89c5e1a2436f3bb7bc
yyujjinn <yujin@ati2000.co.kr>
2023-08-08 13:25:29 +0900

    Iterator Fibonacci

---
+++ chapter4/iterator_fibonacci.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Test
+{
+    static void Main()
+    {
+        foreach (int fib in Fibs(6))
+            Console.Write(fib + " ");
+    }
+
+    static IEnumerable<int> Fibs (int fibCount)
+    {
+        for (int i = 0, prevFib = 1, curFib = 1; i < fibCount; i++)
+        {
+            yield return prevFib;
+            int newFib = prevFib + curFib;
+            prevFib = curFib;
+            curFib = newFib;
+        }
+    }
+}
