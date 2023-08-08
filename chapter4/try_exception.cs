bd77ff0132a0d3a5c710c57c47bcdd6be4a8b2d2
yyujjinn <yujin@ati2000.co.kr>
2023-08-08 11:34:21 +0900

    Try Zero Exception

---
+++ chapter4/try_exception.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Test
+{
+    static int Calc(int x) => 10 / x;
+
+    static void Main()
+    {
+        try
+        {
+            int y = Calc(0);
+            Console.WriteLine(y);
+        }
+        catch (DivideByZeroException ex)
+        {
+            Console.WriteLine("x가 0이면 안 됨");
+        }
+        Console.WriteLine("프로그램 실행 완료");
+    }
+}
