84d571fa0baa65bab0ef3240d1019378db3db0f4
yyujjinn <yujin@ati2000.co.kr>
2023-08-09 08:48:49 +0900

    Custom Binding

---
+++ chapter4/custom_binding.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Dynamic;
+
+public class Test
+{
+    static void Main()
+    {
+        dynamic d = new Duck();
+        d.Quack();              // Quack 메서드가 호출되었음
+        d.Waddle();             // Waddle 메서드가 호출되었음
+    }
+}
+
+public class Duck : DynamicObject
+{
+    public override bool TryInvokeMember (
+        InvokeMemberBinder binder, object[] args, out object result)
+    {
+        Console.WriteLine(binder.Name + " 메서드가 호출되었음");
+        result = null;
+        return true;
+    }
+}
